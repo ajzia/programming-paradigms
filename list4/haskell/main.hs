729305e4228c4a096cd6bb03fffe5a4c712a8064
ajzia <jokul502@wp.pl>
2022-12-05 23:50:09 +0100

    Small changes in diophantine_equation functions

--- list4/haskell/main.hs
+++ list4/haskell/main.hs
@@ -1,16 +1,18 @@
 factorial :: (Eq p, Num p, Enum p) => p -> p
-factorial 0 = 1
 factorial n = product [1..n]
 
 gcd' :: (Integral a) => a -> a -> a
 gcd' a 0 = a
 gcd' a b = gcd' b (a `mod` b)
 
-rec_gcd_extended :: (Integral a) => a -> a -> (a, a, a)
-rec_gcd_extended a 0 = (a, 1, 0)
-rec_gcd_extended a b = (g, y, x - (a `div` b) * y)
-    where (g, x, y) = rec_gcd_extended b (a `mod` b)
+gcd_extended :: (Integral a) => a -> a -> (a, a, a)
+gcd_extended a 0 = (a, 1, 0)
+gcd_extended a b = (g, y, x - (a `div` b) * y)
+                   where (g, x, y) = gcd_extended b (a `mod` b)
 
 diophantine_equation :: (Integral a) => a -> a -> a -> (a, a)
-diophantine_equation a b c = (x, y)
-    where (_, x, y) = rec_gcd_extended a b
+diophantine_equation 0 0 0 = (0, 0)
+diophantine_equation 0 0 _ = error "No solution."
+diophantine_equation a b c | c `mod` g /= 0             = error "No solution."
+                           | otherwise                  = (x * (c `div` g), y * (c `div` g))
+                             where (g, x, y) = gcd_extended a b
